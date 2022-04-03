fb8ef6d030034c590b7510a03421a05bd52f8c43
jinshuhang <68993466+jsh-jsh@users.noreply.github.com>
2022-04-03 12:59:57 +0800

    VER 1.1.0-alpha.4
    *> 任务栏显示

--- modules/functions.h
+++ modules/functions.h
@@ -1,5 +1,5 @@
 /*
-Web-Class-Helper Function Module Header File 1.1.0
+Web-Class-Helper Function Module Header File 1.1.0 
 This source code file is under MIT License.
 Copyright (c) 2022 Class Tools Develop Team
 Contributors: jsh-jsh ren-yc
