0981ded233afe594e9f6f34bd5418a3174c41d57
Yuchen Ren <ldfx_ryc@163.com>
2022-05-09 19:18:30 +0800

    VER 2.0.0-patch.1
    xG Update ".gitignore".
    *D Fix icon resource header file.
    -# Add resource file.

--- resource.h
+++ resource.h
@@ -3,6 +3,7 @@ Web Class Helper Resource Control Header File 2.0.0
 This source code file isn't under any license.
 Generator: Visual Studio 2022
 */
+#define IDI_ICON1 101
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE 101
