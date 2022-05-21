d57ecc1becea8d5f1fd7cc1df0b019cfff3167ce
hjl2011 <100132650+hjl2011@users.noreply.github.com>
2022-05-21 19:46:39 +0800

    VER 2.0.1-alpha.1 -- 截图自定义路径  : 需要 data/pi-save.dat  ! 好像日志有点问题qwq

--- Img.py
+++ Img.py
@@ -2,14 +2,29 @@
 Web Class Helper Image Grabbing Module 2.0.0
 This source code file is under MIT License.
 Copyright (c) 2022 Class Tools Develop Team
-Contributors: jsh-jsh ren-yc
+Contributors: jsh-jsh ren-yc hjl2011
 '''
 import datetime
-import winreg
 from PIL import ImageGrab
+import os.path as p
+import os as o
+import datetime as dt
 
-def get_pictures():
-    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders')
-    return winreg.QueryValueEx(key, "My Pictures")[0]
+f = open('data/pi-save.dat','r')
 
-ImageGrab.grabclipboard().save(get_pictures() + "/" + str(datetime.datetime.now().timestamp()) + ".jpg")
+pi_savepath = str(f.read())
+
+if pi_savepath == "" or not(p.exists(pi_savepath)) :
+    print("Path does not exist.\nPlease modify the value in data/pi-save.dat .")
+    f.close()
+    with open('logs/latest.log','w',encoding = "utf-8") as file :
+        w = dt.datetime.now()
+        file.write("[")
+        file.write(w.strftime("%H:%M:%S"))
+        file.write("] [ERR]: Path does not exist.")
+        file.close()
+    o.system("notepad.exe \"data/pi-save.dat\"")
+    
+else :
+    ImageGrab.grabclipboard().save(pi_savepath + "/" + str(datetime.datetime.now().timestamp()) + ".jpg")
+    f.close()
