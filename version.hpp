f250ac894de367cd31aef7cadf36dd45ffa08c73
Yuchen Ren <ldfx_ryc@163.com>
2022-12-31 20:51:11 +0800

    VER 2.1.1
    *D Bump version to "2.1.1".

--- version.hpp
+++ version.hpp
@@ -5,7 +5,7 @@ Copyright (c) 2022 Class Tools Develop Team
 Contributors: jsh-jsh ren-yc
 */
 #define WCH_VER_MAIN L"2.1.1"
-#define WCH_VER_TYPE 3
+#define WCH_VER_TYPE 0
 #if WCH_VER_TYPE != 0
 	#define WCH_VER_BUILD 1
 #endif
